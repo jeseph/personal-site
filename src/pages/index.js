ea16b4fe979e48bbe8a7cedc325c7d0b0e8ccd4f
jeseph <jeseph@qcmny.com>
2017-07-25 11:41:36 -0400

    Set dir structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,18 @@
-import React from 'react'
-import Link from "gatsby-link"
+import React from 'react';
+import Helmet from 'react-helmet';
+import Link from 'gatsby-link';
 
-export default () => <div>Hello world!</div>
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <Helmet title='Welcome to Jese.ph - Like Joseph with an "E"'>
+          <meta name="description" content="Testing a meta description for jese.ph using React Helmet plugin" />
+        </Helmet>
+        <p>Give me a minute, I just bought the domain yesterday...</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
