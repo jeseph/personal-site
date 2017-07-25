06627988726e980318577555d40c069e037bf9c5
jeseph <jeseph@qcmny.com>
2017-07-24 20:36:56 -0400

    whatever

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -4,7 +4,6 @@ module.exports = {
   siteTitleAlt: 'Jese.ph', // Alternative site title for SEO.
   siteLogo: '/logos/logo-1024.png', // Logo used for SEO and manifest.
   siteUrl: 'https://www.jese.ph', // Domain of your website without pathPrefix.
-  pathPrefix: '', // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
   siteDescription: 'A GatsbyJS stater with Advanced design in mind.', // Website description used for RSS feeds/meta description tag.
   siteRss: '/rss.xml', // Path to the RSS file.
   siteFBAppID: 'SETFORLIVE', // FB Application ID for using app insights
