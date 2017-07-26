0af0ce8ed9e50450405424c034a576c841f72ee0
jeseph <jeseph@qcmny.com>
2017-07-26 12:40:02 -0400

    Google Webmaster Tools

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,8 +6,10 @@ class Home extends React.Component {
   render() {
     return (
       <div>
-        <Helmet title='Welcome to Jese.ph - Like Joseph with an "E"'>
+        <Helmet>
+          <title>Welcome to Jese.ph - Like Joseph with an "E"</title>
           <meta name="description" content="Testing a meta description for jese.ph using React Helmet plugin" />
+          <meta name="google-site-verification" content="YJPic35QSc_3gieRtksOQi4Y4LjxJardvN8LmN59tBs" />
         </Helmet>
         <p>Give me a minute, I just bought the domain yesterday...</p>
       </div>
